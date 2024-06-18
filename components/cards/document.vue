dafc9f7efb765e484818cbbe30b40363a19dcff3
intrstllr <100162882+intrstllr@users.noreply.github.com>
2024-06-19 00:04:17 +0300

    swiper

---
+++ components/cards/document.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="flex flex-col px-4 h-"
+        style="
+            height: 450px;
+            width: 340px;
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px 0px #0000001f;
+        "
+    >
+        <div class="py-5 pr-5 font-bold text-3xl" style="color: #1253a2">{{ props.name }}</div>
+        <div style="flex: 1 1 auto; font-size: 18px">
+            {{ props.text }}
+        </div>
+        <div class="px-4 py-3 text-sm text-white">
+            <!-- <nuxt-link  :to="{ path: props.link   }">
+          <button class=" rounded-xl  h-10 px-5 w-full  bg-primary hover:bg-secondary">Перейти</button> 
+        </nuxt-link> -->
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    name: { type: String, required: true },
+    text: { type: String, required: true },
+    link: { type: String, required: true },
+});
+</script>
+
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-    "./app.vue",
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      primary: "#0584FE",
-      secondary: "#1253A2",
-      black: "#09090b",
-      white: "#fafafa",
-      gray: "#71717a",
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+    ],
+    theme: {
+        extend: {},
+        colors: {
+            primary: "#0584FE",
+            secondary: "#1253A2",
+            black: "#09090b",
+            white: "#fafafa",
+            gray: "#71717a",
+        },
+        screens: {
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+
+            tablet: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            laptop: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            desktop: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
     },
-  },
-  plugins: [],
-  css: ["~/assets/css/tailwind.css"],
+    plugins: [],
+    css: ["~/assets/css/tailwind.css"],
 };
